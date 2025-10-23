f56320d45ad4be29ef5f9810543d0d3fd74eb7c6
coolkid122 <140546530+coolkid122@users.noreply.github.com>
2025-10-22 20:04:38 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -56,4 +56,4 @@ async def process_message(message, session):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
